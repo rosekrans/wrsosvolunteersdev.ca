606e62d7e6af3ce2b16150cf12df1da16d749c8a
rosekrans <vulco.jacques@gmail.com>
2022-12-14 15:50:43 -0600

    calllog fixed

--- app/Http/Controllers/CallLogController.php
+++ app/Http/Controllers/CallLogController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Response;
 
 use App\Models\ContactVeterinarian as Veterinarian;
@@ -16,6 +15,7 @@ use App\Models\SolutionType;
 use App\Models\PostalCode;
 use App\Models\Call;
 use App\Models\CallActivity;
+use App\Models\Input;
 use App\Http\Requests\StoreCallLog;
 use App\Http\Requests\UpdateCallLog;
 use Auth;
@@ -148,13 +148,13 @@ class CallLogController extends Controller
 
     public function searchSpecies($term){
 
-        $search = DB::select("SELECT id, species_name FROM wrsosvolunteersdev_ca.species WHERE species_name LIKE '%".$term. "%' ORDER BY 2 ASC");
+        $search = DB::select("SELECT id, species_name FROM wrsosvolunteers_ca.species WHERE species_name LIKE '%".$term. "%' ORDER BY 2 ASC");
         return Response::json($search);
     }
 
     public function searchVolunteers($term){
 
-        $search = DB::select("SELECT id, concat(first_name,' ', last_name) as Name FROM wrsosvolunteersdev_ca.user_details WHERE concat(first_name,' ', last_name) LIKE '%".$term. "%' ORDER BY 1 ASC");
+        $search = DB::select("SELECT id, concat(first_name,' ', last_name) as Name FROM wrsosvolunteers_ca.user_details WHERE concat(first_name,' ', last_name) LIKE '%".$term. "%' ORDER BY 1 ASC");
         return Response::json($search);
     }
 
