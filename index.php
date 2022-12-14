0c7c99add3ae28473078d2e1efc91574945c602d
unknown <vulco.jacques@gmail.com>
2022-12-14 11:17:17 -0600

    Initial push

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ define('LARAVEL_START', microtime(true));
 | loading any of our classes later on. It feels great to relax.
 |
 */
-require __DIR__.'/../wrsosvolunteer/vendors/autoload.php';
+require __DIR__.'/../wrsosvolunteer/vendor/autoload.php';
 //require __DIR__.'/vendor/autoload.php';
 
 /*
