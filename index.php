6fce2350eecd28fae89753ea9f5c4c1ca0d71375
rosekrans <vulco.jacques@gmail.com>
2022-12-14 11:39:32 -0600

    test3

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ define('LARAVEL_START', microtime(true));
 | loading any of our classes later on. It feels great to relax.
 |
 */
-//require __DIR__.'/../wrsosvolunteer/vendor/autoload.php';
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/../wrsosvolunteer/vendor/autoload.php';
+//require __DIR__.'/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -36,8 +36,8 @@ require __DIR__.'/vendor/autoload.php';
 */
 
 
-$app = require_once __DIR__.'/bootstrap/app.php';
-//$app = require_once __DIR__.'/../wrsosvolunteer/bootstrap/app.php';
+//$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../wrsosvolunteer/bootstrap/app.php';
 /*
 |--------------------------------------------------------------------------
 | Run The Application
