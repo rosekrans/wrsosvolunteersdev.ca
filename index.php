114c26b7cf5365f62ea9c29cec26af573d240281
rosekrans <vulco.jacques@gmail.com>
2022-12-14 12:27:38 -0600

    s

--- index.php
+++ index.php
@@ -36,8 +36,8 @@ require __DIR__.'/../wrsosvolunteer/vendor/autoload.php';
 */
 
 
-$app = require_once __DIR__.'/bootstrap/app.php';
-//$app = require_once __DIR__.'/../wrsosvolunteer/bootstrap/app.php';
+//$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../wrsosvolunteer/bootstrap/app.php';
 /*
 |--------------------------------------------------------------------------
 | Run The Application
